f0d0bb872a410f162989b371deb0aa139b0c6cea
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2026-01-16 19:27:21 +0900

    update python: generate_world_config.py

--- python/generate_world_config.py
+++ python/generate_world_config.py
@@ -31,18 +31,81 @@ except ImportError:
     import cnoid.Body
     import cnoid.Util
 
+def print_config(robot_name, bodyfile_path, offset, joint_list, controller_name, wheeljoints, wheel_controller_name, output=None):
+    """
+    Args:
+        output (optional) : output file-stream. If None, sys.stdout is used
+    """
+    if output is None:
+        import sys
+        output = sys.stdout
+
+    world_config = {'robot':
+                    {
+                        'model': bodyfile_path,
+                        'name': robot_name,
+                        'initial_coords': {'pos': offset},
+                        'initial_joint_angles': [0 for _ in range(num_joint)],
+                        'fix': True,
+                        'BodyROSItem': {'name_space': robot_name},
+                        'ROSControlItem': {'name_space': robot_name}
+                    },
+                    'world':
+                    {
+                        'World': {'name': 'MyWorld'},
+                        'Simulator': {
+                            'type': 'AISTSimulator',
+                            'name': 'AISTSim'
+                        },
+                        'GLVision': None,
+                        'Camera': {
+                            'lookEye': [0.0, 3.0, 1.7],
+                            'lookUp': [0.0, 0.0, 1.0],
+                            'lookAtCenter': [0.0, 0.0, 0.7]
+                        },
+                        'WorldROS': None,
+                        'ROS': {
+                            'generate': {'robot': bodyfile_path,
+                                         'name_space': robot_name,
+                                         'controllers': [
+                                             {
+                                                 'name': controller_name,
+                                                 'type': 'position',
+                                                 'joints': sorted([j.jointName for j in joint_list if j.jointName not in wheeljoints])
+                                             }
+                                         ]
+                                         }
+                        }
+                    },
+                    'objects': [
+                        {
+                            'model': 'choreonoid://share/model/misc/floor.body',
+                            'name': 'MyFloor',
+                            'fix': True
+                        }
+                    ]
+                    }
+    if len(wheeljoints) > 0:
+        world_config['world']['ROS']['generate']['controllers'].append(
+            {'name':wheel_controller_name,
+             'type':'position',
+             'joints': sorted([j.jointName for j in joint_list if j.jointName in wheeljoints])
+             })
+    ###
+    print(yaml.dump(world_config, indent=2, sort_keys=False), file=output)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='generage world setting file',  # プログラム名
         usage='Generate world setting for choreonoidros',  # プログラムの利用方法
         add_help=True,  # -h/–help オプションの追加
     )
-    parser.add_argument('bodyfile', type=str, default="robotname.body")
+    parser.add_argument('--bodyfile', type=str, default="robotname.body")
     parser.add_argument('--robotname', type=str, default="")
     parser.add_argument('--offsetx', type=float, default="0.0")
     parser.add_argument('--offsety', type=float, default="0.0")
     parser.add_argument('--offsetz', type=float, default="0.0")
-    parser.add_argument('--joint_controller_name', type=str, default="joint_controller")
+    parser.add_argument('--joint_controller_name', type=str, default="trajectory_controller")
     parser.add_argument('--wheel_controller_name', type=str, default="wheel_controller")
     parser.add_argument('--wheeljoints', nargs="*", type=str, default=[])
     args = parser.parse_args()
@@ -75,53 +138,7 @@ if __name__ == '__main__':
 
     p = pathlib.Path(args.bodyfile)
     bodyfile_path = str(p.resolve())
-    robotname = args.robotname if args.robotname != "" else rbody.getModelName()
+    robot_name = args.robotname if args.robotname != "" else rbody.getModelName()
 
-    world_config = {'robot':
-                    {
-                        'model': bodyfile_path,
-                        'name': robotname,
-                        'initial_coords': {'pos': [args.offsetx, args.offsety, args.offsetz]},
-                        'initial_joint_angles': [0 for _ in range(num_joint)],
-                        'fix': True,
-                        'BodyROSItem': {'name_space': robotname},
-                        'ROSControlItem': {'name_space': robotname}
-                    },
-                    'world':
-                    {
-                        'World': {'name': 'MyWorld'},
-                        'Simulator': {
-                            'type': 'AISTSimulator',
-                            'name': 'AISTSim'
-                        },
-                        'GLVision': None,
-                        'Camera': {
-                            'lookEye': [0.0, 3.0, 1.7],
-                            'lookUp': [0.0, 0.0, 1.0],
-                            'lookAtCenter': [0.0, 0.0, 0.7]
-                        },
-                        'WorldROS': None,
-                        'ROS': {
-                            'generate': {'robot': bodyfile_path,
-                                         'name_space': robotname,
-                                         'controllers': [
-                                             {
-                                                 'name': args.joint_controller_name,
-                                                 'type': 'position',
-                                                 'joints': sorted([j.jointName for j in joint_list if j.jointName not in args.wheeljoints])
-                                             }
-                                         ]
-                                         }
-                        }
-                    },
-                    'objects': [
-                        {
-                            'model': 'choreonoid://share/model/misc/floor.body',
-                            'name': 'MyFloor',
-                            'fix': True
-                        }
-                    ]
-                    }
-    if len(args.wheeljoints) > 0:
-        world_config['world']['ROS']['generate']['controllers'].append({'name':args.wheel_controller_name, 'type':'position', 'joints': sorted([j.jointName for j in joint_list if j.jointName in args.wheeljoints]) })
-    print(yaml.dump(world_config, indent=2, sort_keys=False))
+    print_config(robot_name, bodyfile_path, [args.offsetx, args.offsety, args.offsetz],
+                 joint_list, args.joint_controller_name, args.wheeljoints, args.wheel_controller_name)
